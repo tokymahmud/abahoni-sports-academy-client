3462b213e4df8bd676111cd1b8c291090201e948
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-06-06 22:59:28 +0600

    error page added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,12 +3,15 @@ import {
   } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
+import ErrorPage from "../Pages/ErrorPage/error-page";
   
 
   export const router = createBrowserRouter([
     {
       path: "/",
       element: <Main></Main> ,
+      errorElement: <ErrorPage />,
+
       children:[
         {
             path: '/',
