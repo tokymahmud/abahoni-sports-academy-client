5114d68117e7c160ac1805f59e38de571c067993
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-06-06 22:53:18 +0600

    navbar footer routes done

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  RouterProvider,
+} from "react-router-dom";
+import { router } from './Routes/Routes.jsx';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+<div className='max-w-screen-xl mx-auto'>
+<RouterProvider router={router} />
+
+</div>
   </React.StrictMode>,
 )
