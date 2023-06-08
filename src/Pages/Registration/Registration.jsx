bfc861d0f181cc91aaa0cd36f89249fa96bdfa8f
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-06-08 22:15:43 +0600

    react hook for added done

--- src/Pages/Registration/Registration.jsx
+++ src/Pages/Registration/Registration.jsx
@@ -11,79 +11,98 @@ const Registration = () => {
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
   console.log(watch("example"));
 
-  const [error, setError] = useState('');
 
   const { createUser } = useContext(AuthContext);
   console.log(createUser);
 
-  const handleRegister = async (data) => {
-    const { name, email, img, password, confirmPassword } = data;
-
-    if (password !== confirmPassword) {
-      setError('Passwords do not match');
+  const onSubmit = data=>{
+    if (data.password !== data.confirmPassword) {
       return;
     }
+  createUser(data.email,data.password)
+  .then(result=>{
+    const loggedUser =result.user;
+    console.log(loggedUser);
 
-    try {
-      const result = await createUserWithEmailAndPassword(auth, email, password);
-      const loggedUser = result.user;
-      console.log(loggedUser);
-      setError('');
-    } catch (error) {
-      console.log(error.message);
-      setError(error.message);
-    }
-  };
+
+  })}
+
 
   return (
-    <div>
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col md:flex-row-reverse">
-          <div className="text-center md:text-left">
-            <h1 className="text-5xl font-bold"> Please Register!</h1>
-          </div>
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form onSubmit={handleSubmit(handleRegister)} className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Name</span>
-                </label>
-                <input type="text" name="name" placeholder="Name" className="input input-bordered" required {...register('name')} />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input type="email" name="email" placeholder="email" className="input input-bordered" required {...register('email')} />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input type="password" name="password" placeholder="password" className="input input-bordered" required {...register('password')} />
-              </div>
-              <div className="form-control">
+    <>
+   
+    <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="text-center lg:text-left">
+                <h1 className="text-5xl font-bold">Sign up now!</h1>
+                <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            </div>
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text"  {...register("name", { required: true })} name="name" placeholder="Name" className="input input-bordered" />
+                        {errors.name && <span className="text-red-600">Name is required</span>}
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Photo URL</span>
+                        </label>
+                        <input type="text"  {...register("photoURL", { required: true })} placeholder="Photo URL" className="input input-bordered" />
+                        {errors.photoURL && <span className="text-red-600">Photo URL is required</span>}
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email"  {...register("email", { required: true })} name="email" placeholder="email" className="input input-bordered" />
+                        {errors.email && <span className="text-red-600">Email is required</span>}
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password"  {...register("password", {
+                            required: true,
+                            minLength: 6,
+                            maxLength: 20,
+                            pattern: /(?=.*[A-Z])(?=.*[!@#$&*])/
+                        })} placeholder="password" className="input input-bordered" />
+                        {errors.password?.type === 'required' && <p className="text-red-600">Password is required</p>}
+                        {errors.password?.type === 'minLength' && <p className="text-red-600">Password must be 6 characters</p>}
+                        {errors.password?.type === 'maxLength' && <p className="text-red-600">Password must be less than 20 characters</p>}
+                        {errors.password?.type === 'pattern' && <p className="text-red-600">Password must have one Uppercase one lower case, one number and one special character.</p>}
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div className="form-control">
                 <label className="label">
                   <span className="label-text">Confirm Password</span>
                 </label>
-                <input type="password" name="confirmPassword" placeholder="confirm password" className="input input-bordered" required {...register('confirmPassword')} />
+                <input
+                  type="password"
+                  {...register("confirmPassword", {
+                    required: true,
+                    validate: (value) => value === watch('password')
+                  })}
+                  placeholder="confirm password"
+                  className="input input-bordered"
+                />
+                {errors.confirmPassword?.type === 'required' && <p className="text-red-600">Confirm Password is required</p>}
+                {errors.confirmPassword?.type === 'validate' && <p className="text-red-600">Passwords do not match</p>}
               </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Image URL</span>
-                </label>
-                <input type="text" name="img" placeholder="Image URL" className="input input-bordered" required {...register('img')} />
-              </div>
-              <button type="submit" className="btn btn-primary">Register</button>
-            </form>
-            <label className="label">
-              <Link to="/login" className="label-text-alt link link-hover">Already have an account?</Link>
-            </label>
-            <p className="text-red-600">{error}</p>
-          </div>
+                    <div className="form-control mt-6">
+                        <input className="btn btn-primary" type="submit" value="Sign Up" />
+                    </div>
+                </form>
+                <p><small>Already have an account <Link to="/login">Login</Link></small></p>
+            </div>
         </div>
-      </div>
     </div>
+</>
   );
 };
 
