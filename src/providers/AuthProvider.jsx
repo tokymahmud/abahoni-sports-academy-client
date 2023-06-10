7981bfa357ab8c444d4070a9c0f2ed5f3a52fef4
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-06-10 20:49:30 +0600

    jwt token

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState } from 'react';
 import {GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut} from "firebase/auth";
 import { app } from '../firebase/firebase.config';
+import axios from 'axios';
 
 export const AuthContext = createContext(null);
 const auth =getAuth(app);
@@ -40,6 +41,18 @@ const AuthProvider = ({children}) => {
        const unsubscribe = onAuthStateChanged(auth,currentUser=>{
             console.log('auth state change', currentUser);
             setUser(currentUser);
+            if(currentUser){
+                axios.post('http://localhost:5000/jwt',{email:currentUser.email})
+                .then(data=>{
+                    // console.log(data.data.token)
+                    localStorage.setItem('access-token',data.data.token)
+                })
+
+            }
+            else{
+                localStorage.removeItem('access-token')
+            }
+           
             setloading(false);
 
         });
